0dd2e2e9aa5005be268394e8d64e74e1a554be09
elisate <dushimiyimanaelisa@gmail.com>
2024-11-12 19:16:27 +0200

    first commit

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  base: "/Ntamupaka-F-W/", // Replace with your repository name
-});
