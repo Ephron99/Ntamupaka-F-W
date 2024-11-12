c025132ed8551d3a786d79dcb9e52252d7514e8e
elisate <dushimiyimanaelisa@gmail.com>
2024-11-12 18:45:51 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Ntamupaka-F-W/", // Add your repository name here
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-  },
+  base: "/Ntamupaka-F-W/", // Replace with your repository name
 });
