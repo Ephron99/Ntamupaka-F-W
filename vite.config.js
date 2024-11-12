7bd149cbf9fa8722fe0c9180820da1cf4a45f041
elisate <dushimiyimanaelisa@gmail.com>
2024-11-12 17:31:45 +0200

    some change in config js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/Ntamupaka-F-W/", // Add your repository name here
   plugins: [react()],
   build: {
     rollupOptions: {
